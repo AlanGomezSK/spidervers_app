caa17d3594c6323591114745bbf3c5e43ad31802
AlanGomezSK <alndiaz@4gmail.com>
2022-06-02 14:04:15 -0500

    adding spiderman.js with the class

---
+++ app/spiderman.js
@@ -0,0 +1,10 @@
+class spiderman  {
+    constructor(name,age,actor,nOfMovies,studio){
+        this.name=name;
+        this.age=age;
+        this.actor=actor;
+        this.nOfMovies=nOfMovies;
+        this.studio=studio;
+    }
+}
+module.exports=spiderman
