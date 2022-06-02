2408a7689a6255da1c769cf0b157290ef7e612c7
AlanGomezSK <alndiaz@4gmail.com>
2022-06-02 14:04:56 -0500

    class remove

---
+++ index.js
@@ -0,0 +1,9 @@
+class spiderman  {
+    constructor(name,age,actor,nOfMovies,studio){
+        this.name=name;
+        this.age=age;
+        this.actor=actor;
+        this.nOfMovies=nOfMovies;
+        this.studio=studio;
+    }
+}
