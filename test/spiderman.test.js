050540dec1d4b0026425ceb2d4a7bb8d00116995
AlanGomezSK <alndiaz4@gmail.com>
2022-06-04 15:01:45 -0500

    add test of TomHolland object

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -2,15 +2,17 @@
 const spiderman = require("../app/spiderman")
 
 describe("unit test for spiderman class", () => {
-    test('Case 1 Create an spiderman object', () => {
+    test('use the method getinfo()', () => {
       
       const andrewGarfield = new spiderman ("Spiderman sony",31,"Andrew Garfield", 3,"Sony")
-      
+      const tomHolland = new spiderman("Spiderman Marvel", 25, "Tom Holland",5,"Disney")
       /*Esto es lo que se quiere obtener */
       expect(andrewGarfield.name).toBe("Spiderman sony");
       expect(andrewGarfield.age).toBe(31);
       expect(andrewGarfield.actor).toBe("Andrew Garfield");
       expect(andrewGarfield.nOfMovies).toBe(3);
       expect(andrewGarfield.studio).toBe("Sony");
+
+      expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
     });
   })
