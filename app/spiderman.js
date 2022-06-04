e119ec286b750aa543a15bdb05c77a79ac91fbf4
AlanGomezSK <alndiaz4@gmail.com>
2022-06-04 15:00:07 -0500

    added the function getInfo()

--- app/spiderman.js
+++ app/spiderman.js
@@ -1,10 +1,13 @@
 class spiderman  {
-    constructor(name,age,actor,nOfMovies,studio){
+    constructor(name,age,actor,nOfMovies,studio,){
         this.name=name;
         this.age=age;
         this.actor=actor;
         this.nOfMovies=nOfMovies;
         this.studio=studio;
+    }
+    getInfo(){
+        return `Hey, I'm Tom Holland from Marvel studio`
     }
 }
 module.exports=spiderman
